980cbacf1ff6dc24f008259a1cf931be69fd368c
tofer17 <tofer17@gmail.com>
2019-03-21 14:47:32 -0500

    First commit of VGT- Card and CardGroup.

---
+++ src/main/webapp/vgt2go/VGTCard.js
@@ -0,0 +1,93 @@
+/**
+ *
+ */
+import { VGTComponent } from "./VGTComponent.js";
+
+class VGTCardBase extends Object {
+	constructor ( id, name, order ) {
+		super();
+		Object.defineProperty( this, "id", { value: id } );
+		Object.defineProperty( this, "name", { value: name } );
+		Object.defineProperty( this, "order", { value: order } );
+	};
+}
+
+class VGTRank extends VGTCardBase {
+	constructor ( id, name, order ) {
+		super( id, name, order );
+	};
+}
+
+
+const RANKS = [];
+RANKS.push( new VGTRank( "A", "Ace", 1 ) );
+RANKS.push( new VGTRank( "2", "Two", 2 ) );
+RANKS.push( new VGTRank( "3", "Three", 3 ) );
+RANKS.push( new VGTRank( "4", "Four", 4 ) );
+RANKS.push( new VGTRank( "5", "Five", 5 ) );
+RANKS.push( new VGTRank( "6", "Six", 6 ) );
+RANKS.push( new VGTRank( "7", "Seven", 7 ) );
+RANKS.push( new VGTRank( "8", "Eight", 8 ) );
+RANKS.push( new VGTRank( "9", "Nine", 9 ) );
+RANKS.push( new VGTRank( "T", "Ten", 10 ) );
+RANKS.push( new VGTRank( "J", "Jack", 11 ) );
+RANKS.push( new VGTRank( "Q", "Queen", 12 ) );
+RANKS.push( new VGTRank( "K", "King", 13 ) );
+
+class VGTSuit extends VGTCardBase {
+	constructor ( id, name, order ) {
+		super( id, name, order );
+	};
+
+	get color () {
+		return this.id == "D" || this.id == "H" ? "red" : (this.id == "C" || this.id == "S" ? "black" : "joker");
+	};
+}
+
+const SUITS = [];
+SUITS.push( new VGTSuit( "C", "Clubs", 1 ) );
+SUITS.push( new VGTSuit( "D", "Diamonds", 2 ) );
+SUITS.push( new VGTSuit( "H", "Hearts", 3 ) );
+SUITS.push( new VGTSuit( "S", "Spades", 4 ) );
+
+const JOKER_SUIT = new VGTSuit( "X", "Joker", 100 );
+
+class VGTCard extends VGTComponent {
+	constructor ( rank, suit ) {
+		super( "div",
+			"vgtcard-" +
+			rank.id + suit.id,
+			"vgtcard", suit.color + "suit"
+		);
+
+		Object.defineProperty( this, "rank", { value: rank } );
+		Object.defineProperty( this, "suit", { value: suit } );
+		Object.defineProperty( this, "id", { value: rank.id + suit.id } );
+	};
+
+	update () {
+		this.node.innerHTML = this.id;
+	};
+
+	toString () {
+		return this.id;
+	};
+
+	static get SUITS () {
+		return SUITS;
+	};
+
+	static get RANKS () {
+		return RANKS;
+	};
+
+	static get JOKER_SUIT () {
+		return JOKER_SUIT;
+	}
+
+	static MakeJoker ( rank ) {
+		return new VGTCard( new VGTRank( rank, "Joker " + rank, 100+rank ), JOKER_SUIT );
+	};
+}
+
+export { VGTCard };
---
+++ src/main/webapp/vgt2go/VGTCardGroup.js
@@ -0,0 +1,89 @@
+/**
+ *
+ */
+import { VGTComponent  } from "./VGTComponent.js";
+import { VGTCard } from "./VGTCard.js";
+
+const _TYPES = {FaceDown : "FD", RTL : "RTL", FaceUp : "FU" };
+
+class VGTCardGroup extends VGTComponent {
+	constructor ( id, type, ...cards ) {
+		super( "div", id, "vgtcardgroup" );
+		this.id = id;
+		this.type = type;
+		this.cards = cards != null ? cards : [];
+		//Object.defineProperty( this, "cards", { value : cards != null ? cards : [] } );
+
+		if ( this.type != null ) {
+			this.node.classList.add( this.type );
+		}
+	};
+
+	removeJokers () {
+		this.cards = this.cards.filter( card => card.suit != VGTCard.JOKER_SUIT );
+	};
+
+	shuffle () {
+		// Adapted from:
+		// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	    for ( let i = this.cards.length - 1; i > 0; i-- ) {
+	        const j = Math.floor( Math.random() * ( i + 1 ) );
+	        [ this.cards[ i ], this.cards[ j ] ] = [ this.cards[ j ], this.cards[ i ] ];
+	    }
+	};
+
+	deal ( count ) {
+		if ( count < 1 ) {
+			return null
+		}
+
+		const ret = [];
+		for ( let i = 0; i < count; i++ ) {
+			ret.push( this.cards.pop() );
+		}
+
+		return count > 1 ? ret : ret[0];
+	}
+
+	// Place SRC at DST.
+	//	SRC=null: remove DST
+	//  DST=null: push SRC
+	reorder ( src, dst ) {
+
+		const to = isFinite( dst ) ? dst : this.cards.indexOf( dst );
+		const from = isFinite( src ) ? src : this.cards.indexOf( src );
+
+		if ( to >= 0 && from >= 0 ) {
+			this.cards.splice(to, 0, this.cards.splice(from, 1)[0]);
+		} else if ( to == -1 && from == -1 ) {
+			this.cards.push( src );
+		} else if ( from == -1 ) { // to >= 0
+			this.cards.splice( to, 0, dst );
+		} else { // to == -1, from >= 0
+			reorder( this.cards, from, this.cards.length - 1 );
+		}
+
+		this.update();
+
+	};
+
+	static get TYPES () {
+		return _TYPES;
+	};
+
+	static MakeStandardDeck () {
+		const cards = [];
+		for ( let suit of VGTCard.SUITS ) {
+			for ( let rank of VGTCard.RANKS ) {
+				//if ( suit.id != "X" && rank.id != "X" ) {
+					cards.push( new VGTCard( rank, suit ) );
+				//}
+			}
+		}
+		cards.push( VGTCard.MakeJoker( 1 ) );
+		cards.push( VGTCard.MakeJoker( 2 ) );
+		return new VGTCardGroup( "standard", _TYPES.FaceDown, ...cards );
+	};
+}
+
+export { VGTCardGroup };
